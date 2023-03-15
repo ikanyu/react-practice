c33a25d58e98dcd55cf062f650d7f66d26f89438
WenYu <wongwy93@gmail.com>
2023-03-16 00:28:46 +0800

    Use formatter

--- 4. sg-2-hour-forecast/src/App.js
+++ 4. sg-2-hour-forecast/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import moment from 'moment';
+import 'moment-timezone';
 
 function App() {
+  const [forecasts, setForecasts] = useState([]);
+
+  function retrieveForecast() {
+    let currentDateTime = moment.tz('Asia/Singapore').format();
+    const formattedDateTime = currentDateTime.replace(/\+.*$/, '');
+
+    const sgWeatherApi =
+      'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2023-03-15T23:00:00&date=2023-03-15';
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Singapore 2 Hour Weather Forecase</h1>
+      <button type="button" onClick={retrieveForecast}>
+        Retrieve Now!
+      </button>
     </div>
   );
 }
