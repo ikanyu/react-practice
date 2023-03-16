470ad3e6d586569f6fbc0a1117c59055c9dbf320
WenYu <wongwy93@gmail.com>
2023-03-16 23:02:38 +0800

    Trigger click to fetch API

--- 4. sg-2-hour-forecast/src/App.js
+++ 4. sg-2-hour-forecast/src/App.js
@@ -1,25 +1,69 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import moment from 'moment';
 import 'moment-timezone';
+import axios from 'axios';
+
+async function retrieveForecast() {
+  let currentDateTime = moment.tz('Asia/Singapore').format();
+  const formattedDateTime = currentDateTime.replace(/\+.*$/, '');
+  const date = moment.tz('Asia/Singapore').format('YYYY-MM-DD');
+
+  const weatherEndpoint =
+    'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=' +
+    formattedDateTime +
+    '&date=' +
+    date;
+
+  try {
+    const response = await axios.get(weatherEndpoint);
+    console.log('success!');
+
+    return response.data.items[0].forecasts;
+  } catch (error) {
+    console.log('We have encountered an error.');
+  }
+}
+
+function List(props) {
+  const isRetrieved = props.isRetrieved;
+  const forecasts = props.forecasts[0];
+
+  if (isRetrieved) {
+    const listItems = forecasts.map((forecast) => (
+      <li>
+        {forecast.area} {forecast.forecast}
+      </li>
+    ));
+    return <ul>{listItems}</ul>;
+  } else {
+    return <></>;
+  }
+}
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
+  const [retrieved, setRetrieved] = useState(false);
 
-  function retrieveForecast() {
-    let currentDateTime = moment.tz('Asia/Singapore').format();
-    const formattedDateTime = currentDateTime.replace(/\+.*$/, '');
-
-    const sgWeatherApi =
-      'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2023-03-15T23:00:00&date=2023-03-15';
+  async function handleClick() {
+    const data = await retrieveForecast();
+    const updatedData = [...forecasts, data];
+    setForecasts(updatedData);
   }
+
+  useEffect(() => {
+    if (forecasts.length) {
+      setRetrieved(true);
+    }
+  }, [forecasts]);
 
   return (
     <div className="App">
-      <h1>Singapore 2 Hour Weather Forecase</h1>
-      <button type="button" onClick={retrieveForecast}>
+      <h1>Singapore 2 Hour Weather Forecast</h1>
+      <button type="button" onClick={handleClick}>
         Retrieve Now!
       </button>
+      <List isRetrieved={retrieved} forecasts={forecasts} />
     </div>
   );
 }
