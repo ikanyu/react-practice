afad15634be32aa2eb41c339f9c97c7d2e464573
WenYu <wongwy93@gmail.com>
2023-04-10 15:21:43 +0800

    Add tailwind and update README

--- 5. quote-generator/pages/index.js
+++ 5. quote-generator/pages/index.js
@@ -32,19 +32,44 @@ export default function Home({ initialQuoteData }) {
 
   return (
     <>
-      <h2>Quote of the Day</h2>
-      <h2>Current Category {category}</h2>
-      {quoteData.map((quote) => (
-        <li>{quote['quote']}</li>
-      ))}
-
-      {categories.map((category) => (
-        <CategoryButton
-          category={category}
-          // onClick={setCategorySelected}
-          setCategorySelected={setCategorySelected}
-        />
-      ))}
+      <div className="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 py-5">
+        <div
+          className="w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800"
+          style={{ maxWidth: 500 }}
+        >
+          <div className="w-full pt-1 pb-5"></div>
+          <div className="w-full mb-10">
+            <div className="text-3xl text-indigo-500 text-left leading-tight h-3">
+              “
+            </div>
+            <p className="text-sm text-gray-600 text-center px-5">
+              {quoteData.map((quote) => (
+                <>{quote['quote']}</>
+              ))}
+            </p>
+            <div className="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">
+              ”
+            </div>
+          </div>
+          <div className="w-full">
+            <p className="text-md text-indigo-500 font-bold text-center">
+              {category.toUpperCase()}
+            </p>
+            <p className="text-xs text-gray-500 text-center">
+              Quote of the Day
+            </p>
+            <></>
+            <div class="flex pt-6">
+              {categories.map((category) => (
+                <CategoryButton
+                  category={category}
+                  setCategorySelected={setCategorySelected}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
